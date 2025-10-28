7e858fe3eaa8e7582193e0023e7bb41428f1a742
Helder Nogueira <heldernogueira.dev@gmail.com>
2025-10-28 09:17:22 -0400

    Broken UPdate, just saving the project online

--- src/ScenesAPI.js
+++ src/ScenesAPI.js
@@ -12,11 +12,35 @@ class ScenesAPI extends APIBase {
 
     init() {
 
-        this.loadScene = this.loadScene.bind(this);
+        this.saveScene = this.saveScene.bind(this);
+        this.fetchScene = this.fetchScene.bind(this);
         this.createScene = this.createScene.bind(this);
 
-        this.addRoute(true, 'post', 'load', this.loadScene);
+        this.addRoute(true, 'post', 'save', this.saveScene);
+        this.addRoute(true, 'post', 'fetch', this.fetchScene);
         this.addRoute(true, 'post', 'create', this.createScene);
+    }
+
+    async saveScene(req, res) {
+
+        const userID = req.user.id;
+        const { projectName, scene } = req.body;
+
+        if(!userID || !projectName || !scene)
+            return this.sendFail(res);
+
+        const sceneConfig = JSON.parse(scene);
+        console.log(sceneConfig);
+        let userPath = path.join('./clients/', String(userID));
+        userPath = path.join(userPath, projectName);
+
+        if(!fs.existsSync(userPath))
+            return this.sendFail(res);
+
+        const scenePath = path.join(userPath, sceneConfig.name);
+        fs.writeFileSync(scenePath, JSON.stringify(sceneConfig));
+
+        return res.status(200).json({ data: this.fsAPI.buildDirTree(userPath, '|', projectName)});
     }
 
     async createScene(req, res) {
@@ -34,13 +58,18 @@ class ScenesAPI extends APIBase {
             return this.sendFail(res);
 
         const scenePath = path.join(userPath, sceneName + ".ws");
+        const sceneConfig = {
+
+            name: sceneName + ".ws",
+            objects: []
+        };
         if(!fs.existsSync(scenePath))
-            fs.writeFileSync(scenePath, "name: " + sceneName);
+            fs.writeFileSync(scenePath, JSON.stringify(sceneConfig));
 
         return res.status(200).json({ data: this.fsAPI.buildDirTree(userPath, '|', projectName)});
     }
 
-    async loadScene(req, res) {
+    async fetchScene(req, res) {
 
         const userID = req.user.id;
         const { projectName, sceneName } = req.body;
